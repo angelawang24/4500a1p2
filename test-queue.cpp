417ba4cda64d55c8b4e8dfe111dc7f33a43b989e
angelawang <wang.ang@husky.neu.edu>
2020-01-30 11:44:41 -0500

    Fix test issues and remove object, string tests

--- test-queue.cpp
+++ test-queue.cpp
@@ -9,42 +9,39 @@ void t_true(bool p) { if (!p) FAIL(); }
 void t_false(bool p) { if (p) FAIL(); }
 
 int main(int argc, char** argv) {
-    // Object tests
     Object* a = new Object();
     Object* b = new Object();
     Object* c = new Object(a);
-    t_true(a->equals(c));
 
-    // String tests
     String* p = new String("hello");
     String* q = new String("world");
     String* r = new String(p);
-    t_true(p->length() == 5);
-    t_false(p->equals(q));
-    t_true(p->equals(r));
-    t_true(p->compare(q) < 0);
-    t_true(p->compare(r) == 0);
 
     // Queue tests
     Queue* x = new Queue();
     Queue* y = new Queue();
     t_true(x->peek() == nullptr);
     t_true(x->dequeue() == nullptr);
+    OK("empty queue tests");
     x->enqueue(p)->enqueue(q);
     Queue* z = new Queue(x);
     t_false(x->equals(y));
     t_true(x->equals(z));
     t_true(x->size() == 2);
+    OK("push queue tests");
     t_true(x->dequeue()->equals(p));
+    OK("pop queue tests");
     y->enqueue(q);
     x->enqueue(p);
     y->enqueue(p);
     t_true(x->equals(y));
     t_true(x->peek()->equals(q));
+    z = new Queue();
     z->enqueue(a)->enqueue(b);
     t_true(z->size() == 2);
     t_true(z->dequeue()->equals(a));
     t_true(z->peek()->equals(b));
+    OK("Everything passed");
 
     return 0;
 }
